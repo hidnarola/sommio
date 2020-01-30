61eafc2aef1952e42f18d9c68b56d204e2ba64d1
Hiren Danecha <hda@narola.email>
2020-01-30 12:10:33 +0530

    Changes

--- src/lambda/order_created.js
+++ src/lambda/order_created.js
@@ -2,19 +2,21 @@ const axios = require('axios')
 
 handler = async event => {
   console.log('Event SSI ============> ', event)
-  const body = JSON.parse(event.body)
-  let data = body.object.user;
-  let item = body.object.items;
-
-  let shipperData =
-    item &&
-    item.filter(i => return i.name === 'Shipping cost')
   const mailgunUrl =
     'https://api.eu.mailgun.net/v3/builton.sommio.co.uk/messages'
   const username = 'api'
   const password = 'key-f4ba739508713f09336d8316a66e1565'
+  const body = JSON.parse(event.body)
+  console.log('body ============> ', body)
+  if (body && body.object) {
+    let data = body.object.user
+    let item = body.object.items
+    let shipperData =
+      item &&
+      item.filter(i => {
+        return i.name === 'Shipping cost'
+      })
 
-  try {
     const response = await axios({
       method: 'post',
       url: mailgunUrl,
@@ -24,34 +26,32 @@ handler = async event => {
         to: body.object.user.email,
         subject: 'Sommio Blanket',
         html: `<html><body>
-                <div>
-                    <h4>Order Details</h4>
-                    <p>Name - ${data.first_name} ${data.last_name} </p>
-                    <p>Order Id - ${body.object._id}</p>
-                  <div>
-                    <h5>Product : </h5>
-                    <p>Product name: ${item[0].name}</p>
-                    <p>Total Quantity : ${item[0].quantity}</p>
-                    <p>Product Price: ${item[0].final_price}</p>
-                    <p>Shiping charge : ${shipperData[0] &&
-                      shipperData[0].final_price}</p>
-                    <p>Total Amount : ${body.object.total_amount}</p>
-                  </div>
-              <div>
-                <h5>Shipping Address</h5>
-                <p>Address - ${body.object.delivery_address.street_name}</p>
-                <p>City - ${body.object.delivery_address.city}</p>
-                <p>County - ${body.object.delivery_address.state}</p>
-                <p>Postcode - ${body.object.delivery_address.zip_code}</p>
-                <p>Country - ${body.object.delivery_address.country} </p>
-              </div>
-              </div>
-          </body></html>`
+          <div>
+              <h4>Order Details</h4>
+              <p>Name - ${data.first_name} ${data.last_name} </p>
+              <p>Order Id - ${body.object._id}</p>
+            <div>
+              <h5>Product : </h5>
+              <p>Product name: ${item[0] && item[0].name}</p>
+              <p>Total Quantity : ${item[0] && item[0].quantity}</p>
+              <p>Product Price: ${item[0] && item[0].final_price}</p>
+              <p>Shiping charge : ${shipperData[0] &&
+                shipperData[0].final_price}</p>
+              <p>Total Amount : ${body.object.total_amount}</p>
+            </div>
+        <div>
+          <h5>Shipping Address</h5>
+          <p>Address - ${body.object.delivery_address.street_name}</p>
+          <p>City - ${body.object.delivery_address.city}</p>
+          <p>County - ${body.object.delivery_address.state}</p>
+          <p>Postcode - ${body.object.delivery_address.zip_code}</p>
+          <p>Country - ${body.object.delivery_address.country} </p>
+        </div>
+        </div>
+    </body></html>`
       }
     }).catch(errors => console.log('mailgun errors => ', errors))
     console.log('mailgun response => ', response)
-  } catch (err) {
-    console.log('err ====>', err)
   }
 
   return {
