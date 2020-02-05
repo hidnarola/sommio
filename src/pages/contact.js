f08577977a77178ff5e83a1106cb7ed3963bab08
Hiren Danecha <hda@narola.email>
2020-02-05 11:08:05 +0530

    update contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -51,7 +51,12 @@ const Contact = () => {
   return (
     <div>
       <h2>Contact Us</h2>
-      <form name="contact-form" netlify>
+      <form
+        name="contact-form"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
         <input name="name" placeholder="Your Name" type="text" />
         <input name="email" placeholder="name@name.com" type="email" />
         <textarea name="message" />
