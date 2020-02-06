69d6636acf5f76261b4273dc5bab6aa7c44828d2
Hiren Danecha <hda@narola.email>
2020-02-06 11:22:59 +0530

    Change

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,14 +5,8 @@ const Contact = () => {
   return (
     <div>
       <h2>Contact Us</h2>
-      <form
-        method="post"
-        action="#"
-        data-netlify="true"
-        name="contact"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" value="contact" name="bot-field" />
+      <form method="post" action="#" data-netlify="true" name="contact">
+        <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" id="name" required />
         <input type="email" name="email" id="email" required />
         <textarea name="message" id="message" rows="4" required />
