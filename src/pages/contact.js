e6c8c22df78a3646bc2feeac68face14b4b0fecb
Hiren Danecha <hda@narola.email>
2020-02-06 15:07:25 +0530

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,26 +3,128 @@ import { Field, Form } from 'react-final-form'
 import axios from 'axios'
 
 const Contact = () => {
-  const handle = () => {
-    console.log('Hiii')
+  const [values, setValues] = useState('')
+
+  const url = 'https://sommio.netlify.com/.netlify/functions/contact_form'
+
+  let data = {
+    Name: values.name,
+    Email: values.email,
+    Subject: values.subject,
+    Message: values.message
   }
-  const handleSubmit = () => {
-    console.log('handleMethod')
-    // axios
+
+  const handleForm = async () => {
+    console.log('handleForm')
+    let response = await axios.post(url, data).catch(error => {
+      console.log('serverless function error => ', error)
+    })
+    console.log('serverless response => ', response)
   }
+
+  const formValues = e => {
+    e.preventDefault()
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+  console.log('contact values =============> ', values)
+
   return (
-    <div>
-      <h2>Contact Us</h2>
-      <form method="post" action="/thankyou" name="contact">
-        {/* <input type="hidden" name="form-name" value="contact" /> */}
-        <input type="text" name="name" id="name" required />
-        <input type="email" name="email" id="email" required />
-        <textarea name="message" id="message" rows="3" required />
-        <button type="submit" onClick={handleSubmit}>
-          Submit
-        </button>
-      </form>
-    </div>
+    <Form onSubmit={handleForm}>
+      {({ handleSubmit, form, submitting, pristine, values }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <div className="frm_grp">
+              <Field name="name">
+                {({ input, meta }) => (
+                  <div className="form-group">
+                    <input
+                      {...input}
+                      type="text"
+                      id="name"
+                      placeholder="Name"
+                      onChange={e => {
+                        input.onChange(e)
+                        if (input.onChange) {
+                          formValues(e)
+                        }
+                      }}
+                    />
+                    <label for="name">Name</label>
+                  </div>
+                )}
+              </Field>
+            </div>
+
+            <div className="frm_grp">
+              <Field name="email">
+                {({ input, meta }) => (
+                  <div className="form-group">
+                    <input
+                      {...input}
+                      type="text"
+                      placeholder="Email"
+                      id="email"
+                      onChange={e => {
+                        input.onChange(e)
+                        if (input.onChange) {
+                          formValues(e)
+                        }
+                      }}
+                    />
+                    <label for="email">Email</label>
+                  </div>
+                )}
+              </Field>
+            </div>
+            <div className="frm_grp">
+              <Field name="subject">
+                {({ input, meta }) => (
+                  <div className="form-group">
+                    <input
+                      {...input}
+                      type="text"
+                      placeholder="Subject"
+                      id="subject"
+                      onChange={e => {
+                        input.onChange(e)
+                        if (input.onChange) {
+                          formValues(e)
+                        }
+                      }}
+                    />
+                    <label for="subject">Subject</label>
+                  </div>
+                )}
+              </Field>
+            </div>
+            <div className="frm_grp">
+              <Field name="message">
+                {({ input, meta }) => (
+                  <div className="form-group">
+                    <input
+                      {...input}
+                      type="text"
+                      placeholder="Message"
+                      id="message"
+                      onChange={e => {
+                        input.onChange(e)
+                        if (input.onChange) {
+                          formValues(e)
+                        }
+                      }}
+                    />
+                    <label for="message">Message</label>
+                  </div>
+                )}
+              </Field>
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        )
+      }}
+    </Form>
   )
 }
 export default Contact
