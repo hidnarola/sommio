2454cfda7384e996c9766832811ea724df7754d5
Hiren Danecha <hda@narola.email>
2020-02-06 11:00:32 +0530

    Minor change

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
       <form method="post" action="#" data-netlify="true" name="contact">
         <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" id="name" required />
-        <input type="text" name="email" id="email" required />
+        <input type="email" name="email" id="email" required />
         <textarea name="message" id="message" rows="4" required />
         <input type="submit" value="Send Message" />
       </form>
