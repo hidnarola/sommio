9cd6ae419ef2de1fa19b8d6474f9913a4f1fb9ff
Hiren Danecha <hda@narola.email>
2020-02-17 15:32:51 +0530

    testing for setting out mail data

--- src/lambda/order_created.js
+++ src/lambda/order_created.js
@@ -19,37 +19,41 @@ handler = async event => {
       item.filter(i => {
         return i.name === 'Shipping cost'
       })
-    let htmlData = `<html><body>
-  <div>
-    <h4>Order Details</h4>
-    <p>Name - ${data.first_name} ${data.last_name} </p>
-    <p>Order Id - ${body.object._id}</p>
-  <div>
-     <h5>Product : </h5>
-    ${item.map(p => (
-      <div>
-        <p>Product name: ${p.name}</p>
-        <p>Total Quantity : ${p.quantity}</p>
-        <p>Product Price: ${p.final_price}</p>
-      </div>
-    ))}
-    <p>
-      Shiping charge :
-      ${shipperData[0] && shipperData[0].final_price}
-    </p>
-    <p>Total Amount : ${body.object.total_amount}</p>
-  </div>
-<div>
-<h5>Shipping Address</h5>
-<p>Address - ${body.object.delivery_address.street_name}</p>
-<p>City - ${body.object.delivery_address.city}</p>
-<p>County - ${body.object.delivery_address.state}</p>
-<p>Postcode - ${body.object.delivery_address.zip_code}</p>
-<p>Country - ${body.object.delivery_address.country} </p>
-</div>
-</div>
-</body>
-</html>`
+    let htmlData = (
+      <html>
+        <body>
+          <div>
+            <h4>Order Details</h4>
+            <p>
+              Name - ${data.first_name} ${data.last_name}{' '}
+            </p>
+            <p>Order Id - ${body.object._id}</p>
+            <div>
+              <h5>Product : </h5>$
+              {item.map(p => (
+                <div>
+                  <p>Product name: ${p.name}</p>
+                  <p>Total Quantity : ${p.quantity}</p>
+                  <p>Product Price: ${p.final_price}</p>
+                </div>
+              ))}
+              <p>
+                Shiping charge : ${shipperData[0] && shipperData[0].final_price}
+              </p>
+              <p>Total Amount : ${body.object.total_amount}</p>
+            </div>
+            <div>
+              <h5>Shipping Address</h5>
+              <p>Address - ${body.object.delivery_address.street_name}</p>
+              <p>City - ${body.object.delivery_address.city}</p>
+              <p>County - ${body.object.delivery_address.state}</p>
+              <p>Postcode - ${body.object.delivery_address.zip_code}</p>
+              <p>Country - ${body.object.delivery_address.country} </p>
+            </div>
+          </div>
+        </body>
+      </html>
+    )
     const response = await axios({
       method: 'post',
       url: mailgunUrl,
